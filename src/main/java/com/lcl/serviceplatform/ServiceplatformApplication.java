b95bb643299304376b61914dc9236dbd962fa4f0
lcl <magicstarlin@126.com>
2019-03-13 16:29:25 +0800

    第一次提交

---
+++ src/main/java/com/lcl/serviceplatform/ServiceplatformApplication.java
@@ -0,0 +1,13 @@
+package com.lcl.serviceplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceplatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceplatformApplication.class, args);
+    }
+
+}
